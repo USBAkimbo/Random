4d0d1fd1caa2d42129e3f449d14978527b2b5d3b
77cla77 <77cla77@gmail.com>
2023-04-29 00:09:28 +0100

    updated setup

--- Android/setup.sh
+++ Android/setup.sh
@@ -2,6 +2,7 @@
 clear
 cd ~
 rm podcast-downloader.sh
+rm pka-downloader.sh
 rm music-downloader.sh
 rm update-all-packages.sh
 rm .profile*
@@ -9,7 +10,7 @@ rm -rf ~/bin
 mkdir ~/bin
 
 # Download main scripts to home folder
-wget https://raw.githubusercontent.com/USBAkimbo/Random/master/Android/podcast-downloader.sh
+wget https://raw.githubusercontent.com/USBAkimbo/Random/master/Android/pka-downloader.sh
 wget https://raw.githubusercontent.com/USBAkimbo/Random/master/Android/music-downloader.sh
 wget https://raw.githubusercontent.com/USBAkimbo/Random/master/Android/update-all-packages.sh
 wget https://raw.githubusercontent.com/USBAkimbo/Random/master/Android/.profile
