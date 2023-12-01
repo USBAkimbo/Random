81f96b164dca286c43bc767a1f43535eb492af10
77cla77@gmail.com <77cla77@gmail.com>
2023-12-01 14:09:17 +0000

    change music downloader to accept stdin from sharing to termux

--- Android/music-downloader.sh
+++ Android/music-downloader.sh
@@ -4,17 +4,14 @@ echo
 echo Version 2022-07-01
 echo
 
-# User input
-read -p "Paste the YouTube Music URL: " youtube
-
 # Create temp folder and cd to it
 echo ===== Creating temp folder =====
 mkdir -p /storage/emulated/0/Download/music-temp
 cd /storage/emulated/0/Download/music-temp
 
-# Download song to temp folder
+# Download song to temp folder from stdin
 echo ===== Downloading song =====
-yt-dlp -f bestaudio -x --audio-format mp3 --audio-quality 0 --add-metadata --embed-thumbnail -o "%(artist)s - %(title)s.%(ext)s" $youtube
+yt-dlp -f bestaudio -x --audio-format mp3 --audio-quality 0 --add-metadata --embed-thumbnail -o "%(artist)s - %(title)s.%(ext)s" $1
 
 # Move song to music directory with new name
 echo ===== Moving song to ~/Download =====
