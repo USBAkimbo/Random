60307ecec76c52f7fd7a5d0f94a18ce11acf73e2
77cla77@gmail.com <77cla77@gmail.com>
2024-08-21 22:14:13 +0100

    add: local updates and changes

--- PowerShell/GTA/Start-GTAReplayGlitch.psm1
+++ PowerShell/GTA/Start-GTAReplayGlitch.psm1
@@ -7,12 +7,10 @@ function Start-GTAReplayGlitch {
 	}
 	
 	Write-Host -ForegroundColor Yellow "Do the heist as normal and escape into the water"
-	Write-Host ""
 
 	$InputReceived = $false
 	while (-not $InputReceived) {
 		$Prompt = Read-Host -Prompt "Are you in the water and ready to disable saving? (y/n)"
-		Write-Host ""
 		if ($Prompt -eq "y") {
 			Write-Host -ForegroundColor DarkGreen "Creating firewall rule to block Rockstar server 192.81.241.171"
 			New-NetFirewallRule -Name "Block GTA" -DisplayName "Block GTA" -Direction Outbound -Action Block -RemoteAddress "192.81.241.171"
@@ -29,35 +27,15 @@ function Start-GTAReplayGlitch {
 		}
 	}
 		
-	Write-Host -ForegroundColor Yellow "Wait until your character hands over the bag and the message 'SAVING FAILED' has popped up in the bottom left"
-	Write-Host ""
+	Write-Host -ForegroundColor Yellow "Wait for the cutscene to finish, then switch to single player"
 
 	$InputReceived = $false
 	while (-not $InputReceived) {
-		$Prompt = Read-Host -Prompt "Are you ready to restart GTA? (y/n)"
-		Write-Host ""
+		$Prompt = Read-Host -Prompt "Are you in single player and are you ready to remove the IP block? (y/n)"
 		if ($Prompt -eq "y") {
-			Write-Host -ForegroundColor DarkGreen "Restarting GTA and removing firewall rule"
-			Write-Host ""
-			Get-Process "GTA*","Rockstar*","SocialClub*","Launcher*" | Stop-Process -Force
+			Write-Host -ForegroundColor DarkGreen "Removing firewall rule"
 			Remove-NetFirewallRule -Name "Block GTA"
-
-			$Seconds = 10
-			Write-Host "Sleeping for $Seconds seconds"
-			Write-Host ""
-
-			1..$Seconds | ForEach-Object {
-				$Percent = $_ * 100 / $Seconds
-				Write-Progress -Activity "Sleeping for $Seconds seconds" -Status "$($Seconds - $_) seconds remaining" -PercentComplete $Percent 
-				Start-Sleep -Seconds 1
-				}
-			
-			Write-Host -ForegroundColor DarkGreen "GTA has been killed and the firewall rule has been removed"
-			Write-Host ""
-			Write-Host -ForegroundColor DarkGreen "Starting GTA"
-			Write-Host ""
-			Start-Process "C:\Users\Conor\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Steam\Grand Theft Auto V.url"
-
+			Write-Host -ForegroundColor DarkGreen "Firewall rule removed"
 			$InputReceived = $true
 		}
 		elseif ($Prompt -eq "n") {
--- PowerShell/New-OneTimeSecret.psm1
+++ PowerShell/New-OneTimeSecret.psm1
@@ -1,19 +1,20 @@
 function New-OneTimeSecret {
-
     # Prompt the user to paste the secret in
-    $Secret = Read-Host -Prompt "Paste in the secret you want to share" -AsSecureString
+	$Secret = Read-Host -Prompt "Paste in the secret you want to share" -AsSecureString
 
     # Convert string from secure back to plain text
     $Hashed = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Secret)
     $PlainText = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($Hashed)
 
-    # Send the data and expire secret in 24 hours
-    $Response = Invoke-RestMethod -Method Post -Uri "https://onetimesecret.com/api/v1/share?secret=$($PlainText)&ttl=86400"
+    # Encode the secret to handle special characters
+    $EncodedSecret = [System.Web.HttpUtility]::UrlEncode($PlainText)
 
-    # Echo the link back to you
+    # Send the data, expiring the secret if it's not opened within 84600 seconds (1 day)
+    $Response = Invoke-RestMethod -Method Post -Uri "https://onetimesecret.com/api/v1/share?secret=$($EncodedSecret)&ttl=86400"
+
+    # Echo the link back to you and copy it to your clipboard
     $Link = "https://onetimesecret.com/secret/" + $Response.secret_key
-    Set-Clipboard $Link
+	Set-Clipboard $Link
     Write-Host $Link
-    Write-Host "Link has been copied to the clipboard"
-
-    }
+	Write-Host "Link has been copied to the clipboard"
+}
