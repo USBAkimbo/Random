5a1ba51bf8baa1803f8795db6f902d0db1afc374
77cla77@gmail.com <77cla77@gmail.com>
2025-07-06 12:44:39 +0100

    test: remove testing script

--- CC-Turtles/test.lua
+++
@@ -1,8 +0,0 @@
--- Refuel using collected wood
-for i = 1, 16 do
-    turtle.select(i)
-    local itemDetail = turtle.getItemDetail(i)
-    if itemDetail and string.find(itemDetail.name, "log") then -- Filter for logs
-        turtle.refuel()
-    end
-end
