367aa7758f7d755b435cd046b059238a058ca567
77cla77@gmail.com <77cla77@gmail.com>
2025-07-06 20:23:50 +0100

    fix: cow farm

--- CC-Turtles/cow-farm.lua
+++ CC-Turtles/cow-farm.lua
@@ -9,8 +9,18 @@
 
 while true do
 
-    turtle.suckUp() -- Pull a stack of wheat from the chest above
     turtle.select(1) -- Select slot 1
+    turtle.suckUp() -- Pull a stack of wheat from the chest above
+    local wheatFound = false -- Search for wheat in any slot in case we didn't pull any in and we have some in our inventory
+    for i = 1, 16 do -- Check all 16 inventory slots
+        turtle.select(i)
+        local item = turtle.getItemDetail()
+        if item and item.name == "minecraft:wheat" then
+            wheatFound = true
+            break -- Wheat found, exit loop and keep it selected
+        end
+    end
+
     for i = 1, 32 do -- Try to feed 32 wheat to cows
         turtle.place()
     end
@@ -19,7 +29,7 @@ while true do
     for i = 1, 32 do -- Try to feed 32 wheat to the other cows
         turtle.place()
     end
-    
+        
     print("Sleeping for 5 mins until cows can breed again")
     os.sleep(300)
     print("Starting feeding cycle")
