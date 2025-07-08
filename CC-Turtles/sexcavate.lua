cbd95861bd2421e337e5d6b6b865adcc5c33f1ed
77cla77@gmail.com <77cla77@gmail.com>
2025-07-08 22:28:48 +0100

    fix: pre-gpt mess

--- CC-Turtles/sexcavate.lua
+++ CC-Turtles/sexcavate.lua
@@ -36,7 +36,7 @@ local function clearBlocks()
     turtle.digDown()
 end
 
-local function dig16x16() -- Assumes we're at the bottom left corner of the 16x16 area
+local function dig16x16x3() -- Assumes we're at the bottom left corner of the 16x16 area
     for layer = 1, 2 do -- TESTING - 2 LAYERS - 3 blocks x 20 layers = 60 blocks dug down
         print("Clearing layer " .. layer .. "")
         for row = 1, 16 do -- There are 16 rows in total
@@ -115,141 +115,14 @@ local function unloadItems()
     turtle.turnRight() -- Face the digging area
 end
 
-
-
-
-
-
-
-
--- After clearing all rows in a layer, we are at the bottom-right corner of the 14x14
-
-        -- Refuel using collected wood
-        for i = 1, 16 do
-            turtle.select(i)
-            local itemDetail = turtle.getItemDetail(i)
-            if itemDetail and string.find(itemDetail.name, "log") then -- Filter for logs
-                turtle.refuel()
-            end
-        end
-
-        -- Deposit items in water stream
-        turtle.dig()
-        turtle.forward()
-        turtle.dig()
-        turtle.forward() -- Hover over the water with at least a 1 block space to safely drop into the water
-        turtle.digDown()
-        for i = 1, 16 do
-            turtle.select(i)
-            turtle.dropDown() -- Drop all items into water or into the hole we just dug (which will drop to water when leaves decay)
-        end
-        turtle.back()
-        turtle.back() -- Return to original starting position
-
-        -- Turn right and move through the farm to start the next layer
-        turtle.turnRight()
-        for i = 1, 13 do
-            turtle.forward()
-        end
-        turtle.turnRight()
-
-        if layer < 11 then -- Dig up 3 blocks only if there are more layers to harvest
-            turtle.digUp()
-            turtle.up()
-            turtle.digUp()
-            turtle.up()
-            turtle.digUp()
-            turtle.up()
-        end
-    end
-end
-
 -- Function end --
 
--- Run an initial harvest to ensure a clean start
-print("Starting initial harvest")
-print("Moving 5 blocks forward to the start of the farm for harvesting")
-for i = 1, 5 do
-    turtle.dig() -- Dig the block we're about to move onto
-    turtle.forward()
-end
-print("Reached harvesting start position")
+start main loop here
 
-print("Starting harvest")
-dig16x16()
-print("Harvest complete")
+dig16x16x3()
+returnToStart()
+unloadItems()
 
-print("Returning home") -- We end up 30 blocks above the bottom right hand corner of the farm, facing into the farm
-for i = 1, 5 do
-    turtle.back() -- Move back 5 spaces
-end
-for i = 1, 30 do
-    turtle.down() -- Move down 30 spaces to end on top of the chest
-end
+now go down 3 and repeat, then go down 6, then go down 9 etc until we see bedrock
 
--- Loop through planting, waiting and harvesting - loops infinitely so long as there's saplings in the chest and the server doesn't restart
-while true do
-    -- We start on top of a chest 5 blocks away from the farm
-    -- The 4 blocks in front of us have water below
-    -- The 5th block is the bottom left corner of the 14x14 farm
-
-    print("Collecting 1 stack of spruce saplings from the starting chest")
-    turtle.select(16)
-    -- Check if saplings were successfully sucked
-    if not turtle.suckDown() then
-        print("ERROR: Failed to collect a stack of spruce saplings from the chest")
-        error("Stopping script")
-    end
-
-    print("Moving 5 blocks forward from the chest")
-    for i = 1, 5 do
-        turtle.forward()
-    end
-    print("Reached planting start position")
-
-    print("Planting saplings")
-    plantSaplings()
-    print("Sapling planting complete")
-
-    print("Returning home")
-    turtle.dig()
-    turtle.forward() -- Move forward 1 to hover over the water
-    turtle.turnRight()
-    for i = 1, 13 do -- Move to the bottom left of the farm
-        turtle.dig()
-        turtle.forward()
-    end
-    turtle.turnLeft() -- Turn to face the chest
-    for i = 1, 4 do
-        turtle.dig()
-        turtle.forward() -- End up back on top of the chest
-    end
-    turtle.turnRight()
-    turtle.turnRight() -- Turn to face the farm
-    print("Returned home")
-
-    print("Waiting for trees to grow (10 mins)")
-    os.sleep(600)
-
-    print("Starting harvest")
-    print("Moving 5 blocks forward to the start of the farm for harvesting")
-    for i = 1, 5 do
-        turtle.dig() -- Dig the block we're about to move onto
-        turtle.forward()
-    end
-    print("Reached harvesting start position")
-
-    print("Starting harvest")
-    dig16x16()
-    print("Harvest complete")
-
-    print("Returning home") -- We end up at the planting level but 30 blocks up, so we just need to go back 5 blocks and go down 30 blocks
-    for i = 1, 5 do
-        turtle.back()
-    end
-    for i = 1, 30 do
-        turtle.down()
-    end
-    print("Cycle complete. Starting next cycle in 5 seconds.")
-    sleep(5) -- Small delay before starting the next cycle
-end
+once we see bedrock, i want the turtle to go back to the chest
