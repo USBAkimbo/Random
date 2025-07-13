b7bc835508f609914718ae3411148b23da430505
77cla77@gmail.com <77cla77@gmail.com>
2025-07-13 15:26:22 +0100

    fix: drop into chest

--- CC-Turtles/stupid-farm.lua
+++ CC-Turtles/stupid-farm.lua
@@ -9,10 +9,11 @@
 --
 -- How it works
 --
--- The turtle simply digs in front and the item drops on the floor
--- It's just like a player breaking the block
--- This works well with sugar cane and bamboo - simply place them on a mud block and put a hopper under it
+-- The turtle simply digs in front and deposits the item into the chest below
+-- This works well with sugar cane and bamboo
 
 while true do
+    turtle.select(1) -- Select slot 1
     turtle.dig()
+    turtle.dropDown()
 end
